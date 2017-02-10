066277671ace510a6105a5005e5eb9a2cfb280b6
Rodney Fulk <rfulk@mail.gvsu.edu>
2017-02-09 22:03:27 -0500

    Added controllers and basic views for Admin, Maintenance and Logbook. Also made up initial models for the Maintenance stuff.

---
+++ CIS467-AMP/Models/Logbook/Type.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace CIS467_AMP.Models.Logbook
+{
+    /// <summary>
+    /// This class is just a type class for the type of logbook entry
+    /// Id - Record ID
+    /// Name - Name of logbook entry type
+    /// </summary>
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CIS467-AMP/Models/Maintenance/ClassOfIssue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS467_AMP.Models.Maintenance
+{
+    /// <summary>
+    /// This is the Class of issue being  repaired (examples - Pipe, Pump, Valve)
+    /// Id - record ID
+    /// Name - Name of Issue Class
+    /// </summary>
+    public class ClassOfIssue
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
