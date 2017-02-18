ce7d44d724f63ea8a51eedaefa5dbfcee0cc5c7b
Jason Bensel <benselj@mail.gvsu.edu>
2017-02-18 16:04:58 -0500

    Added new pages for Stock room and inventory. Edited Logbook links to resemble other pages. Added tables for work orders and Stock room inventory.

--- CIS467-AMP/Controllers/Maintenance/MaintenanceController.cs
+++ CIS467-AMP/Controllers/Maintenance/MaintenanceController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CIS467_AMP.Models;
 
 namespace CIS467_AMP.Controllers.Maintenance
 {
     public class MaintenanceController : Controller
     {
+
+        //Disposable object allowing for data querying.
+        private ApplicationDbContext _context;
+
+        public MaintenanceController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Maintenance
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CurrentWorkOrder()
+        {
+            var workOrders = _context.MaintenanceWorkOrders;
+            return View(workOrders);
         }
     }
 }
---
+++ CIS467-AMP/ViewModels/StockRoom/OrderRequestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS467_AMP.ViewModels.StockRoom
+{
+    public class OrderRequestViewModel
+    {
+        
+    }
+}
