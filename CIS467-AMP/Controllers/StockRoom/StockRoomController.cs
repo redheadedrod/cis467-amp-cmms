9a2d2676ec3b57a1dd580f65ee532b9b256d0ca7
Jason <benzino1337@gmail.com>
2017-03-10 17:16:29 -0500

    Added inventory ordering from table. Began web form, edited some css for pages

--- CIS467-AMP/Controllers/StockRoom/StockRoomController.cs
+++ CIS467-AMP/Controllers/StockRoom/StockRoomController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Data.Entity;
 using System.Web.Mvc;
+using System.Linq;
 using CIS467_AMP.Models;
+using CIS467_AMP.Models.Shared;
+using CIS467_AMP.ViewModels;
 using CIS467_AMP.Models.StockRoom;
 using Microsoft.Ajax.Utilities;
+using CIS467_AMP.ViewModels.StockRoom;
 
 namespace CIS467_AMP.Controllers.StockRoom
 {
@@ -11,7 +15,7 @@ namespace CIS467_AMP.Controllers.StockRoom
      * Author: Jason Bensel
      * Default controller to serve /Views/StockRoom/Index.cshtml
      */
-    public class StockRoomController : System.Web.Mvc.Controller
+    public class StockRoomController : Controller
     {
         //Disposable object allowing for data querying.
         private ApplicationDbContext _context;
@@ -32,19 +36,69 @@ namespace CIS467_AMP.Controllers.StockRoom
             return View();
         }
 
+        /// <summary>
+        /// Retrieves Inventory.cshtml
+        /// </summary>
+        /// <returns>List of current inventory</returns>
         public ActionResult Inventory()
         {
-            var inventory = _context.StockroomInventories;   
+            //Include allows the variant and part object to be loaded with the stockroom inventory
+            var inventory = _context.StockroomInventories.Include(i => i.Variant)
+                                                         .Include(p => p.Part);   
             return View(inventory);
         }
 
-
-        //NOT DONE YET
-        public ActionResult OrderRequest()
+        /// <summary>
+        /// Grabs all of the data required for an order and constructs defaults
+        /// </summary>
+        /// <param name="inventory">Current inventory item selected for order</param>
+        /// <returns>viewmodel with all associated data required</returns>
+        public ActionResult OrderRequest(string StockId, int PartId)
         {
-            var orderRequest = _context.StockroomOrders;
+            var orderRequest = new OrderRequestViewModel();
+            var suppliers = _context.StockroomSupplierPartIndices.Where(x => x.StockRoom.StockId == StockId).Include(s => s.Supplier);
+            var order = new Order();
+            var status = new OrderStatus();
+            var part = new Part();
+
+            part = _context.Parts.FirstOrDefault(x => x.Id == PartId);
+            
+            //set defaults
+            order.OrderNumber = "TEST999";
+            order.OrderPlaced = new System.DateTime();
+            order.OrderExpected = new System.DateTime();
+            order.StatusLastUpDate = new System.DateTime();
+            order.OrderApproved = true;
+
+            //Load status id into order
+            status.Id = 0;
+            order.OrderStatus = status;
+
+            //Insert a default order into db
+            _context.StockroomOrders.Add(order);
+
+            //Query the for the current entry to retrieve the ID of the Order (This is not ideal)
+            order = _context.StockroomOrders.FirstOrDefault(x => x.OrderPlaced == order.OrderPlaced);
+
+            //Load suppleirs into viewmodel
+            orderRequest.Suppliers = suppliers;
+            //Load part into viewmodel
+            orderRequest.Part = part;
+            //Load order defaults into view model
+            orderRequest.Order = order;
+
+
+            //Send data to view for 
             return View(orderRequest);
         }
+
+
+        ////NOT DONE YET
+        //public ActionResult OrderRequest()
+        //{
+        //    var orderRequest = _context.StockroomOrders;
+        //    return View(orderRequest);
+        //}
 
         [HttpPost]
         public ActionResult CreateOrder(Order order)
--- CIS467-AMP/ViewModels/StockRoom/OrderRequestViewModel.cs
+++ CIS467-AMP/ViewModels/StockRoom/OrderRequestViewModel.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using CIS467_AMP.Models.Shared;
+using CIS467_AMP.Models.StockRoom;
 
 namespace CIS467_AMP.ViewModels.StockRoom
 {
     public class OrderRequestViewModel
     {
-        
+        public IQueryable<SupplierPartIndex> Suppliers { get; set; }
+        public Order Order { get; set; }
+        public Part Part { get; set; }
     }
 }
