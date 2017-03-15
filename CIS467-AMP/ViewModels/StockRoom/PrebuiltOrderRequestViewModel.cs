976477d628d9845da6174ce38c268df015f3d659
Rodney Fulk <rfulk@mail.gvsu.edu>
2017-03-15 14:30:13 -0400

    Merged with Jason fork.

---
+++ CIS467-AMP/ViewModels/StockRoom/PrebuiltOrderRequestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CIS467_AMP.Models.Shared;
+using CIS467_AMP.Models.StockRoom;
+using System.Web.Mvc;
+using CIS467_AMP.ViewModels.StockRoom;
+
+namespace CIS467_AMP.ViewModels.StockRoom
+{
+    public class PrebuiltOrderRequestViewModel
+    {
+        public IEnumerable<Supplier> Suppliers { get; set; }
+        public double Cost { get; set; }
+        public StockroomInventory CurrentInventory { get; set; }
+        public Order Order { get; set; }
+        public Part Part { get; set; }
+    }
+}
