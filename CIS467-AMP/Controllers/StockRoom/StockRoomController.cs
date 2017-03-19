e28e4d81f7fb08890b539f1a9b9c3298a0880c90
Jason <benselj@mail.gvsu.edu>
2017-03-19 15:42:31 -0400

    added create order

--- CIS467-AMP/Controllers/StockRoom/StockRoomController.cs
+++ CIS467-AMP/Controllers/StockRoom/StockRoomController.cs
@@ -109,13 +109,7 @@ namespace CIS467_AMP.Controllers.StockRoom
                 }
             }
 
-            //Generate random string for order number
-            Random random = new Random();
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            var orderNumber = new string(
-                Enumerable.Repeat(chars, 20)
-                          .Select(s => s[random.Next(s.Length)])
-                          .ToArray());
+            var orderNumber = getOrderNumber();
 
             DateTime createdDate = DateTime.Now;
 
@@ -164,6 +158,94 @@ namespace CIS467_AMP.Controllers.StockRoom
             var inventory = _context.StockRoomInventories.Where(x => x.OnHand < x.MinRequired || x.OnHand - x.Reserved < x.MinRequired).Include(x => x.ManufacturerPart);
 
             return View(inventory);
+        }
+        
+        public ActionResult CreateLowOrder(FormCollection collection)
+        {
+
+            var orderNumber = getOrderNumber();
+
+            DateTime createdDate = DateTime.Now;
+
+            //Used for dummy orders
+            DateTime expectedDate = createdDate.AddDays(5);
+
+            StockRoomOrder order = new StockRoomOrder()
+            {
+                OrderNumber = orderNumber,
+
+            };
+            return RedirectToAction("LowOrderRequest");
+        }
+
+        public ActionResult OrderRequest()
+        {
+            var inventory = _context.StockRoomInventories;
+
+            var viewModel = new OrderRequestViewModel()
+            {
+                Inventory = inventory
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult CreateNewOrder(FormCollection collection)
+        {
+            var orderNumber = getOrderNumber();
+            DateTime createdDate = DateTime.Now;
+            //Used for dummy orders
+            DateTime expectedDate = createdDate.AddDays(5);
+            var stockRoomId = Convert.ToInt32(collection["Inv.StockId"]);
+            var quantityOrdered = collection["Line.NumberOfItemsOrdered"];
+
+            var stockId = _context.StockRoomInventories.FirstOrDefault(x => x.Id == stockRoomId).StockId;
+            var partNumber = _context.StockRoomInventories.FirstOrDefault(x => x.StockId == stockId).ManufacturerPartId;
+            var supplierId = _context.StockroomSupplierPartIndexes.FirstOrDefault(x => x.ManufacturerPartId == partNumber).StockRoomSupplierId;
+            var contact = _context.StockroomSupplierContacts.FirstOrDefault(x => x.StockRoomSupplierId == supplierId).Id;
+            var order = new StockRoomOrder()
+            {
+                OrderNumber = orderNumber,
+                StockRoomSupplierId = supplierId,
+                StockRoomSupplierContactId = contact,
+                OrderPlaced = createdDate,
+                OrderExpected = expectedDate,
+                StatusLastUpDate = createdDate,
+                StockRoomOrderStatusId = 0,
+                OrderApproved = true
+            };
+
+            _context.StockroomOrders.Add(order);
+            _context.SaveChanges();
+
+
+            var orderId = _context.StockroomOrders.FirstOrDefault(x => x.OrderNumber == orderNumber).Id;
+            var manPartId = _context.StockRoomInventories.FirstOrDefault(x => x.StockId == stockId).ManufacturerPartId;
+            var indexId = _context.StockroomSupplierPartIndexes.FirstOrDefault(x => x.ManufacturerPartId == manPartId).Id;
+            var line = new StockRoomOrderLine()
+            {
+                StockRoomOrderId = orderId,
+                StockRoomSupplierPartIndexId = indexId,
+                NumberOfItemsOrdered = Convert.ToInt32(quantityOrdered),
+                Approved = true
+
+            };
+
+            _context.StockroomOrderLines.Add(line);
+            _context.SaveChanges();
+
+            return RedirectToAction("CreateOrder");
+        }
+
+        private string getOrderNumber()
+        {
+            //Generate random string for order number
+            Random random = new Random();
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var orderNumber = new string(
+                Enumerable.Repeat(chars, 20)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            return orderNumber;
         }
 
     }
