900b54f2d19181ced0d1ff9f80df94e712ac5d15
Jason <benselj@mail.gvsu.edu>
2017-04-09 18:17:40 -0400

    Moved carousel to new about page, added dummy dashboard

--- CIS467-AMP/Controllers/HomeController.cs
+++ CIS467-AMP/Controllers/HomeController.cs
@@ -12,18 +12,9 @@ namespace CIS467_AMP.Controllers
         {
             return View();
         }
-
+        
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
